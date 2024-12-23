42a1fb9c453eba0106423942f57882b0a61f2de1
abush46 <40731418+abush46@users.noreply.github.com>
2024-12-24 01:29:39 +0300

    bg

--- src/components/UserRegistration.js
+++ src/components/UserRegistration.js
@@ -41,84 +41,83 @@ const UserRegistration = () => {
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
-
-       <div className="card" style={{ width: '25rem' }}>
-                <div className="card-body">
-                    <h5 className="card-title text-center">Register</h5>
-      <form>
-      <div className="md-3">
-        <label className="form-label">Name</label>
-        <input
-          type="text"
-          className="form-control"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
+      <div className="card" style={{ width: "25rem", background: "#eee" }}>
+        <div className="card-body">
+          <h5 className="card-title text-center">Register</h5>
+          <form>
+            <div className="md-3">
+              <label className="form-label">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="md-3">
+              <label className="form-label">E-mail</label>
+              <input
+                className="form-control"
+                value={email}
+                onChange={(value) => {
+                  setEmail(value.target.value);
+                }}
+              />
+            </div>
+            <div className="md-3">
+              <label className="form-label">Age</label>
+              <input
+                className="form-control"
+                value={age}
+                onChange={(value) => {
+                  setAge(value.target.value);
+                }}
+              />
+            </div>
+            <div className="md-3">
+              <label className="form-label">Gemder</label>
+              <input
+                className="form-control"
+                value={gender}
+                onChange={(value) => {
+                  setGender(value.target.value);
+                }}
+              />
+            </div>
+            <div className="md-3">
+              <label className="form-label">Password</label>
+              <input
+                className="form-control"
+                value={password}
+                onChange={(value) => {
+                  setPassword(value.target.value);
+                }}
+              />
+            </div>
+            <div className="md-3">
+              <label className="form-label">Health Concerns</label>
+              <input
+                className="form-control"
+                value={healthConcerns}
+                onChange={(value) => {
+                  setHealthConcerns(value.target.value);
+                }}
+              />
+            </div>
+            {/* Form fields for name, email, password, age, gender, and health concerns */}
+            <button
+              onClick={handleRegister}
+              className="btn btn-primary"
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="md-3">
-        <label className="form-label">E-mail</label>
-        <input
-          className="form-control"
-          value={email}
-          onChange={(value) => {
-            setEmail(value.target.value);
-          }}
-        />
-      </div>
-      <div className="md-3">
-        <label className="form-label">Age</label>
-        <input
-          className="form-control"
-          value={age}
-          onChange={(value) => {
-            setAge(value.target.value);
-          }}
-        />
-      </div>
-      <div className="md-3">
-        <label className="form-label">Gemder</label>
-        <input
-          className="form-control"
-          value={gender}
-          onChange={(value) => {
-            setGender(value.target.value);
-          }}
-        />
-      </div>
-      <div className="md-3">
-        <label className="form-label">Password</label>
-        <input
-          className="form-control"
-          value={password}
-          onChange={(value) => {
-            setPassword(value.target.value);
-          }}
-        />
-      </div>
-      <div className="md-3">
-        <label className="form-label">Health Concerns</label>
-        <input
-          className="form-control"
-          value={healthConcerns}
-          onChange={(value) => {
-            setHealthConcerns(value.target.value);
-          }}
-        />
-      </div>
-      {/* Form fields for name, email, password, age, gender, and health concerns */}
-      <button
-        onClick={handleRegister}
-        className="btn btn-primary"
-        type="submit"
-      >
-        Register
-      </button>
-      </form>
     </div>
-</div>
-</div>
   );
 };
 

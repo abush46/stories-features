a6d104a613399bb6894a08e107c3ad73d2b99681
Michael Samson <40731418+abush46@users.noreply.github.com>
2024-12-24 19:48:10 +0300

    Update story.js

--- src/components/story.js
+++ src/components/story.js
@@ -1,38 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const StoryWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  position: relative;
+
 `;
 
-const StoryImage = styled.img`
-  width: 100px;
-  height: 100px;
-`;
-
-const ProgressBar = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: ${(props) => props.progress}%;
-  height: 5px;
-  background-color: rgba(255, 255, 255, 0.8);
-  transition: width 0.3s;
-`;
-
-const Story = ({ image }) => {
+const Story = () => {
   const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) => (prev < 100 ? prev + 1 : 100));
-    }, 30);
-    return () => clearInterval(interval);
-  }, []);
+  
 
   return (
     <header>
